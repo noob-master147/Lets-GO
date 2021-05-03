ae0bce223209f939dcabd8dcf7ea240eddd0bc6f
noob-master147 <divyanshkhandelwal147@gmail.com>
2021-05-03 09:57:28 +0530

    network cli init

---
+++ network-cli/go.mod
@@ -0,0 +1,5 @@
+module network-cli
+
+go 1.16
+
+require github.com/urfave/cli v1.22.5
---
+++ network-cli/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Network CLI")
+	// err := cli.NewApp().Run(os.Args)
+	// if err != nil {
+	// 	fmt.Println("Error: ", err)
+	// 	os.Exit(1)
+	// }
+
+	// app: := cli.NewApp()
+	// app.Name = ""
+
+}
